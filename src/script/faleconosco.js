7c63fb93b55ac8a9367305a42a1b67b81717dd5e
edsondl <edsondl2003@yahoo.com.br>
2023-10-25 19:30:06 -0300

    feat: inserindo javascript no formulário

--- src/script/faleconosco.js
+++ src/script/faleconosco.js
@@ -1,7 +1,7 @@
 // ---------- VALIDAÇÃO NOME ---------- //
 let nomeInput = document.getElementById("nome");
 let nomeLabel = document.querySelector('label[for="nome"]');
-let unomeHelper = document.getElementById("nome-helper");
+let nomeHelper = document.getElementById("nome-helper");
 
 //---------- VALIDAÇÃO email ---------- 
 let emailInput = document.getElementById("email");
@@ -10,35 +10,35 @@ let emailHelper = document.getElementById("email-helper")
 
 
 // Mostrar popup de campo obrigatório
-usernameInput.addEventListener("focus", function (){
-    usernameLabel.classList.add('required-popup')
+    nomeInput.addEventListener("focus", function (){
+    nomeLabel.classList.add('required-popup')
 })
 
-emailInput.addEventListener("focus", function (){
+    emailInput.addEventListener("focus", function (){
     emailLabel.classList.add('required-popup')
 })
 // Ocultar popup de campo obrigatório
-usernameInput.addEventListener('blur', function(){
-    usernameLabel.classList.remove ('required-popup')
+    nomeInput.addEventListener('blur', function(){
+    nomeLabel.classList.remove ('required-popup')
 })
-emailInput.addEventListener("blur", function (){
+    emailInput.addEventListener("blur", function (){
     emailLabel.classList.add('required-popup')
 })
 // Validar valor do input
-usernameInput.addEventListener("change", function(evento){
+    nomeInput.addEventListener("change", function(evento){
     let valor = evento.target.value
-    if(valor.length < 4) {
-    usernameInput.classList.remove('correct');
-    usernameInput.classList.add('error');
-    usernameHelper.innerText='Seu Name deve conter 3 ou mais caracteres'
-    usernameHelper.classList.add('visible');
+    if(valor.length < 3) {
+    nomeInput.classList.remove('correct');
+    nomeInput.classList.add('error');
+    nomeHelper.innerText='Seu Nome deve conter 3 ou mais caracteres'
+    nomeHelper.classList.add('visible');
   } else {
-    usernameInput.classList.remove('error');
-    usernameHelper.classList.remove('visible');
-    usernameInput.classList.add('correct');
+    nomeInput.classList.remove('error');
+    nomeHelper.classList.remove('visible');
+    nomeInput.classList.add('correct');
   }
 })
-emailInput.addEventListener("change", function(evento){
+    emailInput.addEventListener("change", function(evento){
     let valor = evento.target.value
   if(valor.includes('@') && valor.includes('.com')) {
     emailInput.classList.remove('error');
@@ -47,7 +47,7 @@ emailInput.addEventListener("change", function(evento){
   } else {
     emailInput.classList.remove('correct');
     emailInput.classList.add('error');
-    emailHelper.innerText='Seu E-mail deve "@" e ".com"'
+    emailHelper.innerText='Seu E-mail deve conter "@" e ".com"'
     emailHelper.classList.add('visible');
     
   }
