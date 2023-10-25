7d996fde0ef8c0ae723f884e2be0c67cbf622616
felipe <felipecleones999@gmail.com>
2023-10-25 18:52:48 -0300

    add sidebar

---
+++ src/js/global.js
@@ -0,0 +1,41 @@
+function clickMenu(){
+  if(sidebar.style.width == '250px'){
+    sidebar.style.width = '0px';
+  }else sidebar.style.width = '250px';
+}
+
+
+
+
+
+
+
+
+
+// Seleciona o ícone do menu e o input do menu
+
+/*const menuCheckbox = document.getElementsByClassName('material-symbols-outlined');
+console.log(menuCheckbox)
+const sidebar = document.getElementById('sidebar');
+
+// Adiciona um evento de clique ao ícone do menu
+menuIcon.addEventListener('click', () => {
+   menuCheckbox.clickd
+});
+
+// Fecha o sidebar quando um item do menu é clicado
+const sidebarLinks = document.querySelectorAll('#sidebar a');
+sidebarLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        menuCheckbox.clickd = false;
+    });
+});
+
+// Abre e fecha o sidebar
+menuCheckbox.addEventListener('change', () => {
+    if (menuCheckbox.checked) {
+        sidebar.style.width = '250px';
+    } else {
+        sidebar.style.width = '0';
+    }
+}); */
