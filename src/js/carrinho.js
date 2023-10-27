96bdbe0cebaedbb7c4fa0014228f5595fc2a89b5
felipe <felipecleones999@gmail.com>
2023-10-27 20:04:07 -0300

    carrinho adicionado

--- src/js/carrinho.js
+++ src/js/carrinho.js
@@ -3,23 +3,24 @@ const itens = [
         nome:"COMPUTADOR GAMER RGB COMPLETO",
         img:"./assents/icon/produtoPequeno.svg",
         valor:3999.99,
-        quantidade:0
+        quantidade:0,
     },
-    {   id:2,
+    {   id:1,
         nome:"FONTE GAMER",
         img:"./assents/icon/produtoPequeno.svg",
         valor:1999.99,
-        quantidade:0
+        quantidade:0,
     },
-    {   id:3,
+    {   id:2,
         nome:"Monitor",
         img:"./assents/icon/produtoPequeno.svg",
         valor:2999.99,
-        quantidade:0
+        quantidade:0,
     }
 ]
 
-inicializarLoja = () =>{
+//Dar inicio a página index
+inicializar = () =>{
     var containerProdutos = document.getElementById("produtos");
     itens.map((val)=>{
        containerProdutos.innerHTML+=
@@ -29,54 +30,61 @@ inicializarLoja = () =>{
             <h3 class="name">`+val.nome+`</h3>
             <img src=`+val.img+` alt="">
             <h3>Valor <span class="cart-product-price">`+val.valor+`</span></h3>
-            <input type="number" value="2" min="0" class="productQtdInput">
+            <input type="number" value="" min="0" class="productQtdInput">
             <a key ="`+val.id+`" href="#" id="comprar">Adicionar ao carrinho!</a>
-            <button class="btn remove-product-button " >remover</button>
-            
         </div>`
+        
     })
 }
-inicializarLoja()
-
-atualizarCarrinho= () =>{
-
-}
-
-var links = document.getElementById("comprar")
-console.log(itens)
-
-for(var i=0; i<links.length;i++){
-    links[i].addEventListener("click",function(){
-        var key = this.getAttribute('id');
-        itens[key].quantidade++;
-        atualizarCarrinho();
-    })
-}
-
-//Funções 
-const removeProductButtons= document.getElementsByClassName("remove-product-button")
-
-
-removerProduto()
-
-function removerProduto(){
-    for(var i=0; 1< removeProductButtons.length; i++){
-        removeProductButtons[i].addEventListener("click", function(event){
-            event.target.parentElement.parentElement.remove();
-        })
+//atualizar carrinho na página do carrinho
+atualizarCarrinho = () => {
+    var containerCarrinho = document.getElementById("carrinho");
+    containerCarrinho.innerHTML = "";
+    //Mostrar na tela o carrinho
+    itens.map((val, index) => {
+        if (val.quantidade > 0) {
+            containerCarrinho.innerHTML += `
+                <div class="produto">
+                    <div>
+                        <h3 class="name">${val.nome}</h3>
+                        <h3>Valor <span class="cart-product-price">${val.valor}</span></h3>
+                        <p>Quantidade: ${val.quantidade}</p>
+                        <a href="#" class="btn remove-product-button" id="comprar" data-index="${index}">Remover</a>
+                    </div>
+                </div>
+            `;
+        }
+    });
+    
+    //Criar a remorção
+    const removeButtons = document.getElementsByClassName("remove-product-button");
+    
+    for (var i = 0; i < removeButtons.length; i++) {
+        removeButtons[i].addEventListener("click", function() {
+            const index = this.getAttribute('data-index'); // Obtenha o índice do item a ser removido
+            itens[index].quantidade--; 
+            atualizarCarrinho(); // Atualize o carrinho após a remoção
+        });
     }
 }
+// Adicionar item da página inicial ao carrinho
+AdicionarItem=()=>{
+    var add = document.getElementsByTagName("a");
 
-
-
-
-
-
-const productCarts= document.getElementsByClassName("card-cart")
-for (var i =0; i<productCarts.length; i++){
-    
+    for (var i = 0; i < add.length; i++) {
+        add[i].addEventListener("click", function() {
+            let key = this.getAttribute('key');
+            itens[key].quantidade++;
+            atualizarCarrinho();
+            return false;
+        });
+    }
     
 }
 
-productPrice = productCart[i].getElementsByClassName("cart-product-price")[0].innerText
-productQuantity = productCart[i].getElementsByClassName("productQtdInput")[0].value
+
+
+inicializar()
+atualizarCarrinho()
+AdicionarItem()
+
