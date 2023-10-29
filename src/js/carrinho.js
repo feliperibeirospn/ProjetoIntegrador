6a0707e564cfdc43f8c67a683ac9534e193d3427
felipe <felipecleones999@gmail.com>
2023-10-29 14:53:21 -0300

    carrinho adicionado

--- src/js/carrinho.js
+++ src/js/carrinho.js
@@ -1,90 +1,236 @@
 const itens = [
-    {   id:0,
-        nome:"COMPUTADOR GAMER RGB COMPLETO",
-        img:"./assents/icon/produtoPequeno.svg",
-        valor:3999.99,
-        quantidade:0,
+    {
+        id: 0,
+        nome: "COMPUTADOR GAMER RGB COMPLETO",
+        img: "./assets/icon/produtoPequeno.svg",
+        valor: 3999.99,
+        quantidade: 0,
     },
-    {   id:1,
-        nome:"FONTE GAMER",
-        img:"./assents/icon/produtoPequeno.svg",
-        valor:1999.99,
-        quantidade:0,
+    {
+        id: 1,
+        nome: "FONTE GAMER",
+        img: "./assets/icon/produtoPequeno.svg",
+        valor: 1999.99,
+        quantidade: 0,
     },
-    {   id:2,
-        nome:"Monitor",
-        img:"./assents/icon/produtoPequeno.svg",
-        valor:2999.99,
-        quantidade:0,
-    }
-]
-
-//Dar inicio a página index
-inicializar = () =>{
-    var containerProdutos = document.getElementById("produtos");
-    itens.map((val)=>{
-       containerProdutos.innerHTML+=
-        `<div class="produto">
-                
+    {
+        id: 2,
+        nome: "Monitor",
+        img: "./assets/icon/produtoPequeno.svg",
+        valor: 2999.99,
+        quantidade: 0,
+    },
+    {
+        id: 3,
+        nome: "Teclado Mecânico",
+        img: "./assets/icon/produtoPequeno.svg",
+        valor: 149.99,
+        quantidade: 0,
+    },
+    {
+        id: 4,
+        nome: "Mouse Gamer",
+        img: "./assets/icon/produtoPequeno.svg",
+        valor: 79.99,
+        quantidade: 0,
+    },
+    {
+        id: 5,
+        nome: "Headset Gamer",
+        img: "./assets/icon/produtoPequeno.svg",
+        valor: 299.99,
+        quantidade: 0,
+    },
+    {
+        id: 6,
+        nome: "Cadeira Gamer",
+        img: "./assets/icon/produtoPequeno.svg",
+        valor: 599.99,
+        quantidade: 0,
+    },
+    {
+        id: 7,
+        nome: "Placa de Vídeo",
+        img: "./assets/icon/produtoPequeno.svg",
+        valor: 999.99,
+        quantidade: 0,
+    },
+    {
+        id: 8,
+        nome: "SSD 1TB",
+        img: "./assets/icon/produtoPequeno.svg",
+        valor: 199.99,
+        quantidade: 0,
+    },
+    {
+        id: 9,
+        nome: "Webcam HD",
+        img: "./assets/icon/produtoPequeno.svg",
+        valor: 49.99,
+        quantidade: 0,
+    },
+    {
+        id: 10,
+        nome: "Impressora Laser",
+        img: "./assets/icon/produtoPequeno.svg",
+        valor: 299.99,
+        quantidade: 0,
+    },
+    {
+        id: 11,
+        nome: "Roteador Wi-Fi",
+        img: "./assets/icon/produtoPequeno.svg",
+        valor: 59.99,
+        quantidade: 0,
+    },
+    {
+        id: 12,
+        nome: "Caixa de Som Bluetooth",
+        img: "./assets/icon/produtoPequeno.svg",
+        valor: 79.99,
+        quantidade: 0,
+    },
+    {
+        id: 13,
+        nome: "Monitor Ultrawide",
+        img: "./assets/icon/produtoPequeno.svg",
+        valor: 499.99,
+        quantidade: 0,
+    },
+    {
+        id: 14,
+        nome: "Impressora 3D",
+        img: "./assets/icon/produtoPequeno.svg",
+        valor: 399.99,
+        quantidade: 0,
+    },
+    {
+        id: 15,
+        nome: "Câmera DSLR",
+        img: "./assets/icon/produtoPequeno.svg",
+        valor: 899.99,
+        quantidade: 0,
+    },
+    {
+        id: 16,
+        nome: "Microfone USB",
+        img: "./assets/icon/produtoPequeno.svg",
+        valor: 59.99,
+        quantidade: 0,
+    },
+    {
+        id: 17,
+        nome: "Mochila Gamer",
+        img: "./assets/icon/produtoPequeno.svg",
+        valor: 29.99,
+        quantidade: 0,
+    },
+    {
+        id: 18,
+        nome: "Cooler RGB",
+        img: "./assets/icon/produtoPequeno.svg",
+        valor: 19.99,
+        quantidade: 0,
+    },
+    {
+        id: 19,
+        nome: "Tapete para Mouse",
+        img: "./assets/icon/produtoPequeno.svg",
+        valor: 9.99,
+        quantidade: 0,
+    },
+];
+  
+  const containerProdutos = document.getElementById("produtos");
+  const containerCarrinho = document.getElementById("carrinho");
+  
+  function criarProdutoHTML(item) {
+    return `
+      <div class="produto">
         <div>
-            <h3 class="name">`+val.nome+`</h3>
-            <img src=`+val.img+` alt="">
-            <h3>Valor <span class="cart-product-price">`+val.valor+`</span></h3>
-            <input type="number" value="" min="0" class="productQtdInput">
-            <a key ="`+val.id+`" href="#" id="comprar">Adicionar ao carrinho!</a>
-        </div>`
-        
-    })
-}
-//atualizar carrinho na página do carrinho
-atualizarCarrinho = () => {
-    var containerCarrinho = document.getElementById("carrinho");
+          <h3 class="name">${item.nome}</h3>
+          <img src="${item.img}" alt="">
+          <h3>Valor <span class="cart-product-price">${item.valor}</span></h3>
+          <input type="number" value="" min="0" class="productQtdInput">
+          <a key="${item.id}" href="#" class="adicionar-ao-carrinho">Adicionar ao carrinho!</a>
+        </div>
+      </div>
+    `;
+  }
+  
+  function criarCarrinhoHTML(item, index) {
+    if (item.quantidade > 0) {
+      return `
+        <div class="produto">
+          <div>
+            <h3 class="name">${item.nome}</h3>
+            <h3>Valor <span class="cart-product-price">${item.valor}</span></h3>
+            <p>Quantidade: ${item.quantidade}</p>
+            <a href="#" key="${item.id}" class="btn remove-product-button" data-index="${index}">Remover</a>
+          </div>
+        </div>
+      `;
+    }
+  }
+  
+  function atualizarCarrinho() {
     containerCarrinho.innerHTML = "";
-    //Mostrar na tela o carrinho
-    itens.map((val, index) => {
-        if (val.quantidade > 0) {
-            containerCarrinho.innerHTML += `
-                <div class="produto">
-                    <div>
-                        <h3 class="name">${val.nome}</h3>
-                        <h3>Valor <span class="cart-product-price">${val.valor}</span></h3>
-                        <p>Quantidade: ${val.quantidade}</p>
-                        <a href="#" class="btn remove-product-button" id="comprar" data-index="${index}">Remover</a>
-                    </div>
-                </div>
-            `;
-        }
+    itens.forEach((item, index) => {
+      const carrinhoHTML = criarCarrinhoHTML(item, index);
+      if (carrinhoHTML) {
+        containerCarrinho.innerHTML += carrinhoHTML;
+      }
     });
-    
-    //Criar a remorção
-    const removeButtons = document.getElementsByClassName("remove-product-button");
-    
-    for (var i = 0; i < removeButtons.length; i++) {
-        removeButtons[i].addEventListener("click", function() {
-            const index = this.getAttribute('data-index'); // Obtenha o índice do item a ser removido
-            itens[index].quantidade--; 
-            atualizarCarrinho(); // Atualize o carrinho após a remoção
-        });
+    adicionarEventosRemover();
+    salvarCarrinhoNoLocalStorage(); // Chama a função para salvar no localStorage
+  }
+  
+  function adicionarEventosAdicionar() {
+    const botoesAdicionar = document.querySelectorAll(".adicionar-ao-carrinho");
+    botoesAdicionar.forEach((botao) => {
+      botao.addEventListener("click", function () {
+        const key = this.getAttribute('key');
+        itens[key].quantidade++;
+        atualizarCarrinho();
+        return false;
+      });
+    });
+  }
+  
+  function adicionarEventosRemover() {
+    const botoesRemover = document.querySelectorAll(".remove-product-button");
+    botoesRemover.forEach((botao) => {
+      botao.addEventListener("click", function () {
+        const index = this.getAttribute('key');
+        itens[index].quantidade--;
+        atualizarCarrinho();
+      });
+    });
+  }
+  
+  function inicializar() {
+    itens.forEach((item) => {
+      containerProdutos.innerHTML += criarProdutoHTML(item);
+    });
+  
+    adicionarEventosAdicionar();
+    carregarCarrinhoDoLocalStorage(); // Chama a função para carregar do localStorage
+    atualizarCarrinho();
+  }
+  
+  function salvarCarrinhoNoLocalStorage() {
+    const carrinhoJSON = JSON.stringify(itens);
+    localStorage.setItem('carrinho', carrinhoJSON);
+  }
+  
+  function carregarCarrinhoDoLocalStorage() {
+    const carrinhoJSON = localStorage.getItem('carrinho');
+    if (carrinhoJSON) {
+      const carrinhoItens = JSON.parse(carrinhoJSON);
+      itens.forEach((item, index) => {
+        item.quantidade = carrinhoItens[index].quantidade;
+      });
     }
-}
-// Adicionar item da página inicial ao carrinho
-AdicionarItem=()=>{
-    var add = document.getElementsByTagName("a");
-
-    for (var i = 0; i < add.length; i++) {
-        add[i].addEventListener("click", function() {
-            let key = this.getAttribute('key');
-            itens[key].quantidade++;
-            atualizarCarrinho();
-            return false;
-        });
-    }
-    
-}
-
-
-
-inicializar()
-atualizarCarrinho()
-AdicionarItem()
-
+  }
+  
+  inicializar();
